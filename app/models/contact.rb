7ac762eb2490c90a56a8a1340bc53876ee435276
Pravin <pavithraasokan6@gmail.com>
2017-07-19 12:53:14 +0530

    removed duplicate migrations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,14 +6,9 @@ class Contact < ApplicationRecord
   
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-			byebug
-        #Contact.create! row.to_hash 
-        Contact.create!(:name => row["name"], :email => row["email"], :company => row["company"], :tag_list => [row["tag_list"]])
-        
-        puts row["name"]
-        puts row.to_hash
-      end
-    
+    	#Contact.create! row.to_hash 
+      Contact.create!(:name => row["name"], :email => row["email"], :company => row["company"], :tag_list => [row["tag_list"]])
+		end
   end 
 
 end
--- db/migrate/20170718082947_create_templates.rb
+++
@@ -1,8 +0,0 @@
-class CreateTemplates < ActiveRecord::Migration[5.1]
-  def change
-    create_table :templates do |t|
-
-      t.timestamps
-    end
-  end
-end
