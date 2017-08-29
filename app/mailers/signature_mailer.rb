d96bb2754ab494f6b5c2c371ab4a5ece147dea3a
Pravin <pavithraasokan6@gmail.com>
2017-08-29 16:06:25 +0530

    send email based on domain

--- app/mailers/signature_mailer.rb
+++ app/mailers/signature_mailer.rb
@@ -6,7 +6,7 @@ class SignatureMailer < ApplicationMailer
     @paragraph = paragraph
     @contact = Contact.find(contact_id)
     @signature = Signature.find(signature_id)
-    # byebug
+    if (@signature.domain=="joshsoftware.com") then
     options = {
       :user_name => 'apikey',
       :address => @signature.smtp_mail_server,
@@ -16,16 +16,29 @@ class SignatureMailer < ApplicationMailer
       :authentication => 'plain',
       :enable_starttls_auto => true
     }
+  else
+    options = {
+      :user_name => @signature.api_key,
+      :address => @signature.smtp_mail_server,
+      :domain => @signature.domain,
+      :port => '587',
+      :password => @signature.api_key,
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+    }
+  end
     mail(from: @signature.email, to: @contact.email, subject: @subject,delivery_method_options: options)
 
   end
+
   def self_email(signature_id, descriptions,paragraph,subject,greeting)
     @descriptions = descriptions
     @subject = subject
     @greeting = greeting
     @paragraph = paragraph
     @signature = Signature.find(signature_id)
-    # byebug
+     #byebug
+    if (@signature.domain=="joshsoftware.com") then
     options = {
       :user_name => 'apikey',
       :address => @signature.smtp_mail_server,
@@ -33,8 +46,19 @@ class SignatureMailer < ApplicationMailer
       :port => '587',
       :password => @signature.api_key,
       :authentication => 'plain',
-      :enable_starttls_auto => true 
+      :enable_starttls_auto => true
     }
+    else
+    options = {
+      :user_name => @signature.api_key,
+      :address => @signature.smtp_mail_server,
+      :domain => @signature.domain,
+      :port => '587',
+      :password => @signature.api_key,
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+    }
+  end
     mail(from: @signature.email, to: @signature.email, subject: @subject,delivery_method_options: options)
 
   end
