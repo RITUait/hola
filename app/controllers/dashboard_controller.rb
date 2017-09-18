40bad838d301c5e96ea12712a531ee6f67e7c593
Pravin <pavithraasokan6@gmail.com>
2017-09-18 20:29:51 +0530

    show list of conferences with contacts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,19 +2,17 @@ class DashboardController < ApplicationController
   require 'will_paginate/array'
   before_action :authenticate_user!
   def index
-  	@contact = Contact.new
-	  @signature = Signature.new
+    @contact = Contact.new
+    @signature = Signature.new
     #@contacts = Contact.all
     @user = current_user.id
     @contacts = current_user.contacts
     @signatures = current_user.signatures
-    p @c = []
-    @contacts.tag_counts.each do |t| 
-   
-    contacts = current_user.contacts.tagged_with(t.name)
-    @c << {"tag" => t.name, "notsent" => contacts.where(status: false).count, "sent" => contacts.where(status: true).count, "total" => contacts.count } 
+    @c = []
+    @contacts.tag_counts.each do |t|
+      contacts = current_user.contacts.tagged_with(t.name)
+      @c << {"tag" => t.name, "notsent" => contacts.where(status: false).count, "sent" => contacts.where(status: true).count, "total" => contacts.count }
     end
-    p @c
     @page = @c.paginate(page: params[:page], per_page: 10)
     if (@contacts.empty?) && (@signatures.empty?)
       redirect_to home_index_path
