756b70de6e82d56c2655aa0a74311efa6234354d
Pravin <pavithraasokan6@gmail.com>
2017-09-18 20:27:12 +0530

    new user registration process

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,32 +1,32 @@
 class HomeController < ApplicationController
 
-	def index
-		@contact = Contact.new
-		@signature = Signature.new
-	end
+  def index
+    @contact = Contact.new
+    @signature = Signature.new
+  end
 
-	def new
-		@signature = Signature.new
-	end
+  def new
+    @signature = Signature.new
+  end
 
-	def create
+  def create
     @signature = Signature.new(signature_params)
     @signature.user_id = current_user.id
-    p @signature.valid?
+    @signature.valid?
     if (@signature.valid? == false)
-    	render :index
+      render :index
     else
-  		@signature.save
-		end
+      @signature.save
+    end
   end
-    
+
   def contact
-  	@user = current_user.id
+    @user = current_user.id
     @contacts = current_user.contacts   
     @contact = Contact.new(contact_params)
     p @contact.user_id = current_user.id
-      @contact.save
-    
+    @contact.save
+
 
   end
 
@@ -35,7 +35,7 @@ class HomeController < ApplicationController
   end
 
   def contact_params
-  	params.require(:contact).permit(:name,:email,:company,:conference,:tag_list,:user_id)
+    params.require(:contact).permit(:name,:email,:company,:conference,:tag_list,:user_id)
   end
 
- end
+end
