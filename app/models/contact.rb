074bed187d754286f434f151a00d6590bee10548
Pravin <pavithraasokan6@gmail.com>
2017-09-20 10:21:58 +0530

    contact with self import

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,38 +2,28 @@ class Contact < ApplicationRecord
   belongs_to :user
   acts_as_taggable_on :tags
 
-  require 'csv'
 
   def self.import(file,user_id)
     #user = User.find_by(id: user_id)
-    p @errors = []
-    p file
+    @errors = []
     CSV.foreach(file.path, headers: true).with_index  do |row,i|
       i += 1
-      p contact = Contact.find_or_create_by(email: row['email'], user_id: user_id)
+      contact = Contact.find_or_create_by(email: row['email'], user_id: user_id)
       tags = CSV.parse_line(row['conference'], col_sep: ' ')
       if contact.persisted?
-        p contact.tag_list.add(tags)
+        contact.tag_list.add(tags)
         #contact = find_by_tag_list_and_user_id(row["conference"], user)
         contact.save
       else
-        user = User.find_by(id: user_id)
-        contact = Contact.create(:name => row["name"], :email => row["email"], :company => row["company"], :tag_list => tags,:user_id => user_id)
-        if contact.save
-
-        else
-
+        contact = Contact.create(:name => row["name"], :email => row["email"], :company => row["company"], :tag_list => [row["conference"]],user_id: user_id)
+        contact.save
           contact.errors.full_messages.each do |message|
-            p row = [row["name"], row["email"], row["company"], row["conference"], message]
-            message = []
+            row = [row["name"], row["email"], row["company"], row["conference"], message]
             @errors << row
-
-          end
         end
       end
     end
 
-    p @errors
     if @errors.present?
       attributes = %w{Name Email Company Conference}
       CSV.open(Rails.root.join('public', "file.csv"),"wb") do |csv|
