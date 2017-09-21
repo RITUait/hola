6266fecc751827b7660c1b75fbdd7c5b7832502a
Pravin <pavithraasokan6@gmail.com>
2017-09-21 15:02:04 +0530

    email delivery options

--- app/mailers/signature_mailer.rb
+++ app/mailers/signature_mailer.rb
@@ -8,15 +8,15 @@ class SignatureMailer < ApplicationMailer
   def test_email(contact_id, signature_id, descriptions,subject,greeting)
     data(signature_id, descriptions,subject,greeting)
     @contact = Contact.find(contact_id)
-     
+
 
     mail(from: @signature.email, to: @contact.email, subject: @subject,delivery_method_options: @signature.delivery_options)
 
   end
 
   def self_email(signature_id, descriptions,subject,greeting)
-     data(signature_id, descriptions,subject,greeting)
-     #byebug
+    data(signature_id, descriptions,subject,greeting)
+    #byebug
     p @signature.smtp_mail_server
     mail(from: @signature.email, to: @signature.email, subject: @subject,delivery_method_options: options)
 
