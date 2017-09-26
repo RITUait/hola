79e0862abfc2d7ad4a23639e63cf639192222c72
Pravin <pavithraasokan6@gmail.com>
2017-09-26 14:30:43 +0530

    added functionality for delivery options

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -2,22 +2,21 @@ class Signature < ApplicationRecord
   belongs_to :user
 
   def delivery_options
-  	options = {
+    options = {
       :address => smtp_mail_server,
       :port => '587',
       :password => api_key,
+      :domain => email.split('@').last,
       :authentication => 'plain',
       :enable_starttls_auto => true
     }
-    
+
     if (smtp_mail_server =="smtp.sendgrid.net")
       options[:user_name] ='apikey'
-      #options[:domain] = "joshsoftware.com"
     else
       options[:user_name] = api_key
-      #options[:domain] => "simplysmart.tech"
-  	end
-  	options
+    end
+    options
   end
   validates :name,:api_key,:smtp_mail_server, presence: true
   validates_uniqueness_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,:message => '%{value} has already been taken'
