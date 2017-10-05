998136a991728483bb73f6b0a1741b43c1dacc1f
Pravin <pavithraasokan6@gmail.com>
2017-10-05 09:33:34 +0530

    add contact and create signatures

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,11 +1,13 @@
 CKEDITOR.editorConfig = function (config) {
  
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline",  "Strike", "Font","FontSize", "-", "Line Height", "Subscript",  "Superscript", "Smiley", "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock" ],
+    ["Bold",  "Italic",  "Underline",  "Strike", "Font","FontSize", "-", "Line Height","Indent","Outdent","Subscript",  "Superscript", "Smiley", "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock" ],
   ];
   config.toolbar = "mini";
   removePlugins ='elementspath';
   config.removePlugins = 'elementspath';
+  config.disableNativeSpellChecker = false;
+  config.enterMode = CKEDITOR.ENTER_BR;
   config.toolbarCanCollapse = true;
   config.resize_enabled = true
   
