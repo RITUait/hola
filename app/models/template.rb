434c4c937d7ec4d3467b1ce0c389d5203c7ebe6a
Pravin <pavithraasokan6@gmail.com>
2017-10-05 10:09:50 +0530

    validates title and description

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,5 +1,5 @@
 class Template < ApplicationRecord
-	belongs_to :user
+  belongs_to :user
   validates :description, presence: true
-   validates_uniqueness_of :title,:message => '%{value} has already been taken'
+  validates_uniqueness_of :title,:message => '%{value} has already been taken'
 end
