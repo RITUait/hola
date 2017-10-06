b4881f8b4b8c9193afc0d30342453d45d14dff20
Pravin <pavithraasokan6@gmail.com>
2017-10-06 13:07:07 +0530

    added functionaity for text editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,14 +1,16 @@
 CKEDITOR.editorConfig = function (config) {
  
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline",  "Strike", "Font","FontSize", "-", "Line Height","Indent","Outdent","Subscript",  "Superscript", "Smiley", "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock" ],
+    ["Bold",  "Italic",  "Underline",  "Strike", "Font","FontSize","TextColor","-", "Link","Line Height","Indent","Outdent","Subscript",  "Superscript", "Smiley", "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock" ],
   ];
   config.toolbar = "mini";
   removePlugins ='elementspath';
   config.removePlugins = 'elementspath';
   config.disableNativeSpellChecker = false;
   config.enterMode = CKEDITOR.ENTER_BR;
+  config.height = '190px';
   config.toolbarCanCollapse = true;
+  config.statusbar = false;
   config.resize_enabled = true
   
 }
--- app/views/home/signature.html.rb
+++
@@ -1,9 +0,0 @@
-<div class="container">
-<%= bootstrap_form_for @contact do |f| %>
-  <%= f.text_field :name %>
-  <%= f.text_field :email %>
-  <%= f.text_field :company %>
-  <%= f.text_field :tag_list,placeholder: 'Enter the conference name'%>
-  <%= f.submit "Save" %>
-<% end %>
-</div>
